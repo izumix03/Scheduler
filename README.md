b16aefee7305778cb14d5b7bca443e5856b6f5e3
izumix03 <takahiro.izumikawa@gmail.com>
2022-06-24 15:24:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## メモ
+- 依存モジュールは `local` 以下
+  - モジュールを利用してPerlスクリプトを利用する場合は `carton exec` を利用する
+
+## 起動
+```shell
+carton exec -- perl -Ilib script/scheduler-server
+```
+
+```shell
+carton exec -- plackup -Ilib -R ./lib --access-log /dev/null -p 5000 -a ./script/scheduler-server
+```
+
+## SQLite のマイグレーション
+```shell
+sqlite3 db/development.db < sql/sqlite.sql
+```
--- lib/Scheduler/Web/Dispatcher.pm
+++ lib/Scheduler/Web/Dispatcher.pm
@@ -3,20 +3,44 @@ use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
+use Time::Piece;
 
 any '/' => sub {
     my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
+    my $order = lc $c->req->parameters->{order} eq 'reverse' ? 'date' : 'date DESC';
+    my @schedules = $c->db->search('schedules', {}, { order_by => $order });
+
+    return $c->render('index.tx', { schedules => \@schedules });
+};
+
+get '/user' => sub {
+    my ($c) = @_;
+    return $c->render('user.tx', {
+        name  => "takahiro",
+        hobby => "training",
+        age   => 36
     });
 };
 
-post '/reset_counter' => sub {
-    my $c = shift;
-    $c->session->remove('counter');
+post '/post' => sub {
+    my ($c) = @_;
+    my $title = $c->req->parameters->{title};
+    my $date = $c->req->parameters->{date};
+
+    my $date_epoch = Time::Piece->strptime($date, '%Y/%m/%d')->epoch;
+    $c->db->insert(schedules => {
+        title => $title,
+        date  => $date_epoch,
+    });
+
+    return $c->redirect('/');
+};
+
+post '/schedules/:id/delete' => sub {
+    my ($c, $args) = @_;
+    my $id = $args->{id};
+
+    $c->db->delete('schedules' => { id => $id });
     return $c->redirect('/');
 };
 
